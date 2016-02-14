84b7a49baeb526f071daf8a181202bfcfb93b2cb
Jonathan Oh <jonoh@YELP-JONOH.local>
2016-02-14 09:07:42 -0800

    improve player ux

---
+++ PlayerSprite.swift
@@ -0,0 +1,61 @@
+//
+//  PlayerSprite.swift
+//  SKPlayGround
+//
+//  Created by Jonathan Oh on 2/13/16.
+//  Copyright © 2016 Jonathan Oh. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class PlayerSprite {
+
+    private var _playerSpeed : Double = 100
+    private var _playerSprite = SKSpriteNode(imageNamed: "Spaceship")
+    private var _playerSize = CGSize(width: 20, height: 20)
+    
+    var playerSpeed : Double {
+        get {
+            return _playerSpeed
+        }
+        set {
+            if newValue == 100 || newValue == 150 || newValue == 200 || newValue == 500 {
+                _playerSpeed = newValue
+            }
+        }
+    }
+    
+    var playerSprite : SKSpriteNode {
+        get {
+            return _playerSprite
+        }
+    }
+    
+    var playerSize : CGSize {
+        get {
+            return _playerSize
+        }
+    }
+    
+    init(parentFrame : CGRect) {
+        //Initializing player speed and size relative to screen size for consistent game play experience
+        _playerSpeed = Double(parentFrame.width)/10.0
+        _playerSprite.size = CGSize(width: Double(parentFrame.width)/51.0, height: Double(parentFrame.width)/51.0)
+        _playerSprite.position = CGPointMake(parentFrame.width/2, parentFrame.height/10)
+    }
+
+    func updatePlayerPosition(currentScene: GameScene, touches: Set<UITouch>) {
+        if let location = touches.first?.locationInNode(currentScene) {
+            let xDist = (self.playerSprite.position.x - location.x)
+            let yDist = (self.playerSprite.position.y - location.y)
+            let distanceOfPoints = sqrt(( xDist * xDist ) + ( yDist * yDist ))
+            let dynamicDistance : Double = (Double(distanceOfPoints) / self.playerSpeed)
+            let actionMove = SKAction.moveTo(location, duration: dynamicDistance)
+            self.playerSprite.runAction(actionMove)
+        } else {
+            print("No Location Found!")
+        }
+    }
+    
+}
--- SKPlayGround/GameScene.swift
+++ SKPlayGround/GameScene.swift
@@ -10,32 +10,39 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    
+    //This variable is used to prvide better UX.  Will need a better solution.
+    var touchesBeganCounter = 0
     var lilSpacey : PlayerSprite!
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        self.view?.multipleTouchEnabled = false
         self.backgroundColor = SKColor.whiteColor()
         
         lilSpacey = PlayerSprite(parentFrame: self.frame)
         
-        //lilSpacey.playerSprite.position = CGPointMake(self.frame.width/2, self.frame.height/2)
         self.addChild(lilSpacey.playerSprite)
         
+        //Needs to be refactored to its own class
+        let map = SKSpriteNode(color: UIColor.redColor(), size: CGSizeMake(self.frame.width, self.frame.height/2))
+        map.position = CGPointMake(self.frame.width/2, self.frame.height/2)
+        self.addChild(map)
         
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        touchesBeganCounter++
+        lilSpacey.playerSprite.removeAllActions()
         lilSpacey.updatePlayerPosition(self, touches: touches)
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        lilSpacey.updatePlayerPosition(self, touches: touches)
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        lilSpacey.playerSprite.removeAllActions()
+        touchesBeganCounter--
+        if touchesBeganCounter == 0 {
+            lilSpacey.playerSprite.removeAllActions()
+        }
     }
     
    
